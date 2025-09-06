795430cc572e49d8d66e7c02fea1d70259270071
aldvnian <138926924+aldvnian@users.noreply.github.com>
2025-09-06 21:47:45 +0100

    Adding Card file

---
+++ Card.py
@@ -0,0 +1,13 @@
+class Card():
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def get_suit(self):
+        return self.suit
+    
+    def get_value(self):
+        return self.value
+    
+    def print_card(self):
+        print("  " + self.value + " - " + self.suit)
